f25398ee21cbffc876853e0381cf72a85c843b01
Sang <gnatnoh123@googlemail.com>
2019-02-06 13:52:52 +0000

    Made the input script work with vector of vectors

--- C++/composition.cxx
+++ C++/composition.cxx
@@ -1,5 +1,4 @@
 /* --------------------------------------------------------------------------------
-
   ---------------------------------------------------------------------------------
   |Calculate the Clustering of the Mixed Lipid Bilayer through a DBSCAN Algorithm | 
   ---------------------------------------------------------------------------------
@@ -78,29 +77,32 @@ void regionQuery(double Distcriteria, double (*COMref) (double)) {
 #include <cerrno>
 #include <cstdlib> 
 #include <cmath>
-#include <Eigen>
-
-#include <Eigen/Dense>
 #include <boost/progress.hpp>
 
-using namespace Eigen;
+//#include <Eigen>
+//#include <Eigen/Dense>
+//using namespace Eigen;
 
 //double CenterOfMass(double* H7, double* H6_1, double* H6_2, double* T3_1, double* T3_2, double* T3_3, double* T4);
 //double trueDist(double* COM1x, double* COM1y, double* COM1z, double* COM2x, double* COM2y, double* COM2z);
 
 /* Function to calculate the centre of mass of each molecule */
 
+const int numberOfPolymers = 1000; // The number of polymers of each type - C12E2 or mimic 
+const int numberofatoms = 71313; // Total number of beads in the simulation
+const int indexCG = 7;
+int numberofSS = 100; /*The number of screenshots in the dump file*/
+const int boxdim = 3;
+
+struct C12E2_skeleton {                                                                                                                              
+  int index[7];
+};
+
+
 double CenterOfMass(double* H7, double* H6_1, double* H6_2, double* T3_1, double* T3_2, double* T3_3, double* T4) {  
   // Need to update
-  double H7coord = H7;
-  double H6coord_1 = H6_1;
-  double H6coord_2 = H6_2;
-  double T3coord_1 = T3_1; 
-  double T3coord_2 = T3_2; 
-  double T3coord_3 = T3_3; 
-  double T4coord = T4; 
   double COM; 
-  COM = (( H7coord ) + ( H6coord_1 ) + ( H6coord_2 ) + ( T3coord_1 )  + ( T3coord_2 )  +( T3coord_3 )  + ( T4coord )) / 7; 
+  COM = (*H7 + *H6_1 + *H6_2 + *T3_1 + *T3_2 + *T3_3 + *T4)/7; 
   /* With this COM definition we now know the COM in each cartesian coordinate */ 
   return COM; 
 }  
@@ -122,12 +124,12 @@ void die (const char *message) {
 //pointCluster* regionQuery (int , double*, double*, double*, double*, double*, double*);
 //#define minClust 5 // the minimum sizes cluster we want to count as a cluster
 class compute {
-
 public:
-  compute(); // Default constructor
-  compute(std::string file); // constructor reading file
-  
+  compute() {}; // Default constructor
+  compute(std::string file) {}; // constructor reading file
+
   void storeFile() {    
+    boost::progress_display show_progress(numberofSS);
     // open file for reading 
     ipf = fopen("dump.final", "r");  // Needs correction 
     // check for error opening file */
@@ -140,11 +142,19 @@ public:
       //  printf("This is the data for trajectory no %d \n", SSno); 
       l = 0;
       n = 0;
+      index = 0;
+      a.clear();
+      b.clear();
+      xco.clear();
+      yco.clear();
+      zco.clear();
       for (int k = 0; k < nlines; k++) { 
 	// get a line from the file 
 	// fgets() returns NULL when it reaches an error or end of file  
+
 	fgets(line,sizeof(line),ipf);
 	//while (fgets(line,sizeof(line),ipf) != NULL) {
+
 	if (l < 5) {	
 	  /* We are doing nothing */
 	  //	printf("%s l= %d", line,l);
@@ -166,26 +176,41 @@ public:
 	  //	printf(" ***** l = %d \n",l );
 	  /* convert the text to numbers */
 	  sscanf(line,"%d %d %lf %lf %lf",&index,&atomtype,&x,&y,&z);
-	  a[SSno].push_back(index); // Push back indices 
-	  b[SSno].push_back(atomtype); // Push back atomtypes
-	  xco[SSno].push_back(x*boxlength[0]); // Push back boxlengths - x coordinates
-	  yco[SSno].push_back(y*boxlength[1]); // Push back boxlengths - y coordinates
-	  zco[SSno].push_back(z*boxlength[2]); // Push back boxlengths - z coordinates
+	  //std::cout << index << " " << atomtype << " " << x << " " << y << " " << z; 
+	  a.push_back(index); // Push back indices 
+	  b.push_back(atomtype); // Push back atomtypes
+	  xco.push_back(x*boxlength[0]); // Push back boxlengths - x coordinates
+	  yco.push_back(y*boxlength[1]); // Push back boxlengths - y coordinates
+	  zco.push_back(z*boxlength[2]); // Push back boxlengths - z coordinates
 	  n++;
 	  l++;
 	  //printf("%d %d %lf %lf %lf\n",index,atomtype,x,y,z);
 	}
       }
+      aTotal.push_back(a);
+      bTotal.push_back(b);
+      xcoTotal.push_back(xco);
+      ycoTotal.push_back(yco);
+      zcoTotal.push_back(zco);
+      ++show_progress;
     }
   }
 
+  void printVectorElements() {
+    for (unsigned int i = 0; i < xcoTotal.size(); ++i)
+      {
+	for (unsigned int j = 0; j < xcoTotal[i].size(); ++j)
+	  {
+	    std::cout << i << " " << j << " " << " " << xcoTotal[i][j] << std::endl;
+	  }
+	
+	//std::cout << std::endl;
+      }
+  }
+  
+  /*
   void CompositionProfile() {        
-    /*      
-    
-	    Calculating the composition profile 
-    
-    */
-    
+    //Calculating the composition profile 
     for (int i = 0; i <= sizeof(C12E2_struct)/sizeof(C12E2_struct[1])-1; i++) { // TODO
 
       //double truedist(double COM1x, double COM1y, double COM1z, double COM2x, double COM2y, double COM2z) {
@@ -203,19 +228,21 @@ public:
       }
       
       // This time, working with mimics 
-       if(trueDist(xco[71312], yco[71312], zco[71312], headGroupMIMICxCOM[i], headGroupMIMICyCOM[i], headGroupMIMICzCOM[i] ) <= 25.000) { 
+       if(trueDist(xco[71312], yco[71312], zco[71312], headGroupMIMCxCOM[i], headGroupMIMICyCOM[i], headGroupMIMICzCOM[i] ) <= 25.000) { 
 	 if (headGroupMIMICzCOM[i] > zco[71312]) {
 	   mimictophead++;
 	 }
 	 else if (headGroupMIMICzCOM[i] < zco[71312]) {
 	   mimicdownhead++;
-	 }
-       }  
-    }
+	   }
+	 }  
+       }
+    
     
     //std::cout << SSno << " " << tophead/(tophead + mimictophead) << " " << mimictophead/(tophead + mimictophead) << " " << downhead/(downhead + mimicdownhead) << " " << mimicdownhead/(downhead + mimicdownhead) << " " << (tophead-mimictophead)/(tophead + mimictophead) << " " << (downhead-mimicdownhead)/(downhead + mimicdownhead) << " " << ((tophead-mimictophead)/(tophead + mimictophead) + (downhead-mimicdownhead)/(downhead + mimicdownhead))/2 << " " <<  ((tophead-mimictophead)/(tophead + mimictophead) - (downhead-mimicdownhead)/(downhead + mimicdownhead))/2 <<   std::endl; 
-  }
-  
+    }
+  */
+ 
   void AllocationVec() {
     /*
       The way that the C12E2 and C12E2-M batches are allocated in the simulation follows
@@ -227,18 +254,23 @@ public:
     */
     
     for (int i = 0; i <= 249; i++) {
+
       // C12E2 batches
+
       int batch1 = 0;
       int batch2 = 3500;
       int batch3 = 7000;
       int batch4 = 10500;
+
       // C12E2-M batches
+
       int batch1M = 1750;
       int batch2M = 5250;	
       int batch3M = 8750;
       int batch4M = 12250;
       
       // Allocate index
+
       A7 = 7*(i);
       A6_1= 7*(i) + 1;
       A6_2= 7*(i) + 2;
@@ -402,6 +434,7 @@ public:
     }
   }
 
+  /*
   double ChainOrderAnalysis(double* H7_x, double* H7_y, double* H7_z, double* H6_1_x, double* H6_1_y, double* H6_1_z,  double* H6_2_x, double* H6_2_y, double* H6_2_z, double* T3_1_x, double* T3_1_y, double* T3_1_z,  double* T3_2_x, double* T3_2_y, double* T3_2_z, double* T3_3_x, double* T3_3_y, double* T3_3_z, double* T4_x, double* T4_y, double* T4_z) {
 
     // This bit is obsolete - needs to be modified
@@ -415,8 +448,10 @@ public:
     double T4coord = T4; 
     double COM;
   }
-   
-    void clusterAnalysis(vector<double>* C12E2xCOM, vector<double>* C12E2yCOM, vector<double>* C12E2zCOM, std::vector<C12E2_skeleton>* values, double* distance, int* PolymerCounter) { 
+  */
+  
+  /*
+  void clusterAnalysis(std::vector<double>* C12E2xCOM, vector<double>* C12E2yCOM, vector<double>* C12E2zCOM, std::vector<C12E2_skeleton>* values, double* distance, int* PolymerCounter) { 
       for (unsigned i : indices(C12E2xCOM)) { // 
 	for (unsigned j : indices(C12E2xCOM)) { // 
 	  if (C12E2xCOM[i] != C12E2xCOM[j] && C12E2yCOM[i] != C12E2yCOM[j] && C12E2zCOM[i] != C12E2zCOM[j]) {	    
@@ -431,13 +466,21 @@ public:
 	}
       }
     }
+  */
 private:    
   // Vectors to store trajectory values 
-  std::vector< std::vector<double>> xco; 
-  std::vector< std::vector<double>> yco; 
-  std::vector< std::vector<double>> zco; 
-  std::vector< std::vector<int>> a;
-  std::vector< std::vector<int>> b;
+
+  std::vector<double> xco; 
+  std::vector<double> yco; 
+  std::vector<double> zco; 
+  std::vector<int> a;
+  std::vector<int> b;
+
+  std::vector< std::vector<double>> xcoTotal; 
+  std::vector< std::vector<double>> ycoTotal; 
+  std::vector< std::vector<double>> zcoTotal; 
+  std::vector< std::vector<int>> aTotal;
+  std::vector< std::vector<int>> bTotal;
   //double xco[numberofatoms],yco[numberofatoms],zco[numberofatoms]; 
   /* ditto for a and b, which represent index and atomtype respectively */
   //int a[numberofatoms],b[numberofatoms];
@@ -467,11 +510,9 @@ private:
   int A13_2, A12_1_2, A12_2_2, A9_1_2, A9_2_2, A9_3_2, A10_2; // Second Batch
   int A13_3, A12_1_3, A12_2_3, A9_1_3, A9_2_3, A9_3_3, A10_3; // Third Batch
   int A13_4, A12_1_4, A12_2_4, A9_1_4, A9_2_4, A9_3_4, A10_4; // Fourth Batch
-  
-  const int numberOfPolymers = 1000; // The number of polymers of each type - C12E2 or mimic 
-  const int numberofatoms = 71313; // Total number of beads in the simulation
-  const int indexCG = 7;
-  const int numberofSS = 1000; /*The number of screenshots in the dump file*/
+  struct C12E2_skeleton C12E2_struct[numberOfPolymers];                                                                                              
+  struct C12E2_skeleton C12E2M_struct[numberOfPolymers];    
+ 
 
   double tophead = 0;
   double downhead = 0;
@@ -480,9 +521,8 @@ private:
   
   int MimicCounter = 0;    
   int PolymerCounter = 0;    
-  int boxdim = 3;
   int atomtype; 
-  int l, n;
+  int index, l, n;
   int nlines = numberofatoms + 9;      
   double x,y,z; /*coordinates for the atoms in the box*/
   double box1;
@@ -491,7 +531,13 @@ private:
   char line[100];  
 };
 
+compute A;
+  
 int main (int argc, char *argv[])  {
+  
+  A.storeFile();
+  A.printVectorElements();
+  
   return 0;    
 }
 
