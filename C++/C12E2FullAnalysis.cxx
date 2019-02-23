942f8a0dd8178cbd8917b4dfd6f68e37480f94f9
Sang <gnatnoh123@googlemail.com>
2019-02-23 20:39:30 +0000

    renamed composition.cxx

--- C++/C12E2FullAnalysis.cxx
+++ C++/C12E2FullAnalysis.cxx
@@ -454,33 +454,7 @@ private:
   
   FILE *ipf; /* input file */  
   // COM vectors - C12E2
-  std::vector<double> headGroupC12_E2xCOM; // COM C12E2 X
-  std::vector<double> headGroupC12_E2yCOM; // COM C12E2 Y
-  std::vector<double> headGroupC12_E2zCOM; // COM C12E2 Z
-  // COM vectors - C12E2-M
-  std::vector<double> headGroupMIMICxCOM; // COM MIMIC X
-  std::vector<double> headGroupMIMICyCOM; // COM MIMIC Y
-  std::vector<double> headGroupMIMICzCOM; // COM MIMIC Z
-  
-  // -----------------------------------------------------------------------//
-  // Defining normals (A7 -- A6_1 -- A6_2 -- A3_1 -- A3_2 -- A3_3 -- A4     //
-  // -----------------------------------------------------------------------//
-  int A7, A6_1, A6_2, A3_1, A3_2, A3_3, A4; // First Batch
-  int A7_2, A6_1_2, A6_2_2, A3_1_2, A3_2_2, A3_3_2, A4_2; // Second Batch
-  int A7_3, A6_1_3, A6_2_3, A3_1_3, A3_2_3, A3_3_3, A4_3; // Third Batch 
-  int A7_4, A6_1_4, A6_2_4, A3_1_4, A3_2_4, A3_3_4, A4_4; // Fourth Batch
-  
-  // ----------------------------------------------------------------------//
-  // Defining mimics (A13 -- A12_1 -- A12_2 -- A9_1 -- A9_2 -- A9_3 -- A10 //
-  // ----------------------------------------------------------------------//  
-  int A13, A12_1, A12_2, A9_1, A9_2, A9_3, A10; // First Batch
-  int A13_2, A12_1_2, A12_2_2, A9_1_2, A9_2_2, A9_3_2, A10_2; // Second Batch
-  int A13_3, A12_1_3, A12_2_3, A9_1_3, A9_2_3, A9_3_3, A10_3; // Third Batch
-  int A13_4, A12_1_4, A12_2_4, A9_1_4, A9_2_4, A9_3_4, A10_4; // Fourth Batch
 
-  C12E2_skeleton C12E2_struct[numberOfPolymers];                                                                                              
-  C12E2_skeleton C12E2M_struct[numberOfPolymers];    
- 
   double tophead = 0;
   double downhead = 0;
   double mimictophead = 0;
