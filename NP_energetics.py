07751890974cf5135ab4ea58bde12be6df6e2d9a
Sang <gnatnoh123@googlemail.com>
2019-02-23 17:09:08 +0000

    Added additional parts to the membrane coding

--- NP_energetics.py
+++ NP_energetics.py
@@ -6,16 +6,22 @@ import scipy
 
 class curvatureEnergetics:
 	def __self__(self, L, C, Csp, sigma, A, kappa):
-		self.L = L
-		self.C = C
-		self.Csp = Csp
-		self.A = A 
-		self.sigma = sigma
-		self.kappa = kappa
+		self.L = L # Radii?
+		self.C = C # Curvature 
+		self.Csp = Csp # Spontaneous Curvature
+		self.A = A  # Area 
+		self.sigma = sigma # Line tension 
+		self.kappa = kappa # Bilayer Rigidity 
 	def E_edge(self):
+		"""
+		Equation from reference - 
+		"""
 		E = (2 * self.sigma * np.pi * self.L) * np.sqrt(1 - (np.pow((self.L * self.C)/2),2))
 		return E
 	def E_bend(self):
+		"""
+		Equation from reference - 
+		"""
 		E = (0.5 * self.A * self.kappa) * np.pow(((self.L * self.C) - (self.L*self.Csp)), 2)
 		return E
 	
