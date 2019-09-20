2223d535d3ad637dfd8da21dbd5449b2edb05277
Sang <gnatnoh123@googlemail.com>
2019-09-20 17:47:20 +0100

    updated NP code

--- hydrophobicmismatchanalysis.py
+++ hydrophobicmismatchanalysis.py
@@ -2,20 +2,21 @@ import tqdm
 from tqdm import tqdm
 import pandas as pd
 from pandas import DataFrame
-cols = ['Number', 'Name' , 'Residue' ,'ResidueID', 'X', 'Y', 'Z', 'Time' ,'Area' ,' Thick']
 
+cols = ['Number','Name','Residue','ResidueID','X','Y','Z','Time','Area','Thick']
 data = open("DPPCDUPCCHOLaplvoro.txt", 'r')
 lines = data.readlines()
 N = 1124
 outputData = []
 indices = [] 
+
 for index, line in tqdm(enumerate(lines)):
 	data = []
 	if line.split(' ')[0] == '#Framenumber':
 		indices.append(index)
 
 
-for index in range(1,len(indices)):
+for index in tqdm(range(1,len(indices))):
 	data = []
 	for ind in range(indices[index-1]+1, indices[index]):
 		print(indices[index-1], indices[index])
@@ -23,11 +24,15 @@ for index in range(1,len(indices)):
 	outputData.append(data)
 
 	
-for ind1, data in enumerate(outputData):
+for ind1, data in tqdm(enumerate(outputData)):
 	replacementdata = []
 	for line in data:
 		line = [i for i in line.split(' ') if i is not '']
 		replacementdata.append(line)
 	outputData[ind1] = pd.DataFrame(replacementdata, columns = cols)
 	
-		
+
+tempdat = outputData[0]
+types = list(set(tempdat['Residue']))
+NPdat = tempdat[tempdat['Residue'] == 'NP']
+NPindices = list(set(NPdat['ResidueID']))
